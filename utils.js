c069a63d6953aae1fc5c0f4dcd8127fbc1ba1b56
leakrullp <leakrull@proton.me>
2026-03-23 08:40:23 +0100

    Add utils.js script and enhance printShift function for better logging

--- utils.js
+++ utils.js
@@ -3,5 +3,7 @@ const shifts = JSON.parse(localStorage.getItem("shifts")) || [];
 window.printShift = (index) => {
   if (!index) {
     console.log(JSON.stringify(shifts, null, 2));
+  } else {
+    console.log(JSON.stringify(shifts[index - 1], null, 2));
   }
 };
